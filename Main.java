bc75a23e2116ec6a7125645475a1c781493ae09f
Biplob Hasan Khan <134400763+biplobkhan69@users.noreply.github.com>
2023-09-16 17:20:57 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+/**
+ * Main
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -7,5 +10,35 @@ public class Main {
         for (int i = 0; i < 5; i++) {
             System.out.println(a[i]);
         }
+    }
+}
+
+
+   // Initialize the array elements with values
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+    
+        int [] a=new int [5];
+         a[0] = 10;
+         a[1] = 20;
+         a[2] = 30;
+         a[3] = 40;
+         a[4] = 50;
+         
+         a[3]=89;
+
+         
+        for (int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+             System.out.println("length" +a.length);
+             
+        
+
     }
 }
