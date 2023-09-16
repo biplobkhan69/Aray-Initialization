c2e1e7db6ba17c1303926f72a473767b9908c1fb
Biplob Hasan Khan <134400763+biplobkhan69@users.noreply.github.com>
2023-09-16 11:14:37 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+    
+        int[] a = {1, 2, 3, 4, 5}; // Initialize the array with specific values
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
